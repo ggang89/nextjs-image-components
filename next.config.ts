2dada954fc5cd8bce93ea798694b3d89e89b6f7b
ggang89 <ggang89@gmail.com>
2025-03-01 20:29:38 +0900

    외부에서 이미지 가져와서 쓰기(src에 url넣고 next.config.ts에 이미지 경로 지정하기

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: { 
+    // 외부에서 이미지 가져올 때 경로 지정해줘야 함
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.vercel.com",
+        port: "",
+        pathname: "/image/upload/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
