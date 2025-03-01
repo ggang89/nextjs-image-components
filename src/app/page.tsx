0c0f02fad5b5d5e4d004e3e287ab4e86db1684cf
ggang89 <ggang89@gmail.com>
2025-03-01 20:38:49 +0900

    내부폴더 이미지 불러오기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import vercel from "../../public/vercel.png";
 
 export default function Home() {
   return (
     <div>
+      <h2 className="text-center text-2xl">외부에서 가져온 이미지</h2>
       {/* 외부에서 이미지 가져오기 */}
       {/* width, heigh 필수 지정 */}
       <Image
@@ -15,6 +17,9 @@ export default function Home() {
           height: "auto",
         }}
       />
+
+      <h2 className="text-center text-2xl">내부 폴더 이미지 불러오기</h2>
+      <Image src={vercel} alt="Vercel Logo" width={400} height={200} />
     </div>
   );
 }
